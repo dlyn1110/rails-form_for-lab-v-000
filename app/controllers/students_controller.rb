cc277ed48abc59147ec377150101980ae8328a06
Dani Lyn <dlyn1110@gmail.com>
2018-10-18 21:19:09 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,12 +1,12 @@
 class StudentsController < ApplicationController
 
   def new
-       @student = Student.new 
+       @student = Student.new
    end
-   
+
    def create
        @student = Student.new(post_params)
-       @student.save 
+       @student.save
 
        redirect_to student_path(@student)
    end
