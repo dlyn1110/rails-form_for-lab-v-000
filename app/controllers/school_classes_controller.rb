ccc2d9b2b1d7c127694f55517dc94733f2bbe336
Dani Lyn <dlyn1110@gmail.com>
2018-10-18 20:22:20 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -11,10 +11,11 @@ class SchoolClassesController < ApplicationController
   end
 
   def show
-    @school_class = SchoolClass.find(params:id)
+    @school_class = SchoolClass.find(params[:id])
   end
 
   def edit
+    @school_class = SchoolClass.find(params[:id])
   end
 
   def update
