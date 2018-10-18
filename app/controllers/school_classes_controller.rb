fe59a29d9cdca04af1819db08f0e1cd4965ce0ce
Dani Lyn <dlyn1110@gmail.com>
2018-10-18 20:24:08 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -19,7 +19,15 @@ class SchoolClassesController < ApplicationController
   end
 
   def update
+   @school_class = SchoolClass.find(params[:id])
+   @school_class.update(post_params(:title, :room_number))
+   redirect_to school_class_path(@school_class)
   end
 
+  private
 
-end
+  def post_params(*args)
+    params.require(:school_class).permit(*args)
+  end
+
+  end
